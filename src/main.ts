c2dff1c160a39e4a0617bc8695e9728ea1d824dc
benjamin-franklin <felkfranklin@gmail.com>
2020-09-21 22:23:40 +0300

    make module 3

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
+import { stringOne, stringTwo } from './task2/file1';
+import fileAlert from './task2/file2';
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
 }).$mount('#app');
+
+fileAlert(stringOne);
+fileAlert(stringTwo);
---
+++ src/task2/file1.js
@@ -0,0 +1,2 @@
+export const stringOne = 'Это стррока';
+export const stringTwo = 'ТОже строка';
